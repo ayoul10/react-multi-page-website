bf6dc8e1e1a2dd417507ead1e6b432ef0fc1b2fb
ayoul10 <45453703+ayoul10@users.noreply.github.com>
2021-01-02 21:46:00 +0100

    sexy

--- src/backoffice/NewForm.js
+++ src/backoffice/NewForm.js
@@ -1,143 +1,153 @@
 import React, {Component} from "react";
-import {Redirect} from "react-router-dom";
 
 class NewForm extends Component {
     constructor(props) {
         super(props);
+
         this.state = {
-            islogged: false,
-            apiParams: {
-                firstNew:"",
-                title:"",
-                imageSrc:"",
-                date: "",
-                description: "",
-                link: ""
-            }
-        };
+            firstNew:'',
+            title:'',
+            imageSrc:'',
+            date: '',
+            description: '',
+            link: ''
+        }
     }
 
-    SumbitNewArticle = event => {
+    handleTitleChanged(event) {
+        this.setState({
+            title: event.target.value
+        });
+    }
+    handleFirstNewChanged(event) {
+        this.setState({
+            firstNew: event.target.value
+        });
+    }
+    handleimageSrcChanged(event) {
+        this.setState({
+            imageSrc: event.target.value
+        });
+    }
+    handleDateChanged(event) {
+        this.setState({
+            date: event.target.value
+        });
+    }
+    handleDescriptionChanged(event) {
+        this.setState({
+            description: event.target.value
+        });
+    }
+    handleLinkChanged(event) {
+        this.setState({
+            link: event.target.value
+        });
+    }
+
+    handleButtonClicked() {
+        const payload = {
+            firstNew:this.state.firstNew,
+            title:this.state.title,
+            imageSrc:this.state.imageSrc,
+            date:this.state.date,
+            description: this.state.description,
+            link: this.state.link
+
+        };
+        fetch('https://d9qoarugmf.execute-api.eu-west-1.amazonaws.com/api/news?key=11223344',{
+            method: 'POST',
+            headers:{
+                'Accept':'application/json',
+                'Content-type':'application/json'
+            },
+            body: JSON.stringify(payload)
+        })
+            .then(res=>res.json())
+            .then((data)=>{
+                console.log(data);
+            });
+    }
+    DeleteAll = event => {
 
         event.preventDefault();
 
-        let firstNew = this.state.apiParams.firstNew;
-        let title= this.state.apiParams.title;
-        let imageSrc = this.state.apiParams.imageSrc;
-        let date = this.state.apiParams.date;
-        let description = this.state.apiParams.description;
-        let link = this.state.apiParams.link;
+        fetch('https://d9qoarugmf.execute-api.eu-west-1.amazonaws.com/api/news?key=11223344',{
+            method: 'DELETE',
+            headers:{
+                'Accept':'application/json',
+                'Content-type':'application/json'
+            },
+        })
+            .then(res=>res.json())
+            .then((data)=>{
+                console.log(data);
+            });
 
     };
+
     render() {
         return (
             <div className="container">
-                <form onSubmit={this.SumbitNewArticle} className="form-signin">
-                    <div className="row">
-                        <div className="col">
-                            <input
-                                type="text"
-                                name="title"
-                                placeholder="Title"
-                            />
-                            <input
-                                type="text"
-                                name="imageSrc"
-                                placeholder="Image Source"
-                            />
-                            <input
-                                type="text"
-                                name="date"
-                                placeholder="Date"
-                            />
-                            <input
-                                type="text"
-                                name="description"
-                                placeholder="Description"
-                            />
-                            <input
-                                type="text"
-                                name="link"
-                                placeholder="Link"
-                            />
-                            <input
-                                type="boolean"
-                                name="firstNew"
-                                placeholder="firstNew"
-                            />
-                            <input type="submit" value="Login" />
-                        </div>
-                    </div>
+                <div>
+                    <input
+                        type="text"
+                        name="title"
+                        placeholder="Title"
+                        onChange={this.handleTitleChanged.bind(this)}
+                        value={this.state.title}
+
+                    />
+                    <input
+                        type="text"
+                        name="title"
+                        placeholder="Title"
+                        onChange={this.handleTitleChanged.bind(this)}
+                        value={this.state.title}
+
+                    />
+                    <input
+                        type="text"
+                        name="title"
+                        placeholder="Title"
+                        onChange={this.handleTitleChanged.bind(this)}
+                        value={this.state.title}
+
+                    />
+                    <input
+                        type="text"
+                        name="title"
+                        placeholder="Title"
+                        onChange={this.handleTitleChanged.bind(this)}
+                        value={this.state.title}
+
+                    />
+                    <input
+                        type="text"
+                        name="title"
+                        placeholder="Title"
+                        onChange={this.handleTitleChanged.bind(this)}
+                        value={this.state.title}
+
+                    />
+                    <input
+                        type="text"
+                        name="title"
+                        placeholder="Title"
+                        onChange={this.handleTitleChanged.bind(this)}
+                        value={this.state.title}
+
+                    />
+                    <button onClick={this.handleButtonClicked.bind(this)}>
+                        Submit
+                    </button>
+                </div>
+                <form onSubmit={this.DeleteAll} >
+                    <input type="submit" value="Delete All Articles" />
                 </form>
             </div>
         );
     }
 }
 
-/*
-class NewForm extends Component{
-    key = 11223344;
-
-    postData = event => {
-            event.preventDefault();
-        try {
-            let firstNews;
-            let myForm= document.getElementById('submit-form');
-            let formData= new FormData(myForm);
-            firstNews = !!'on'.localeCompare((formData.get('firstNew').toString()));
-            let result = fetch('https://d9qoarugmf.execute-api.eu-west-1.amazonaws.com/api/news?key=11223344',{
-                    method: 'post',
-                    headers:{
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(
-                        {
-                            firstNew: firstNews,
-                            title: formData.get('title'),
-                            imageSrc: formData.get('imageSrc'),
-                            date: formData.get('date'),
-                            description: formData.get('description') ,
-                            link: formData.get('link')
-                        }
-                    )
-                });
-
-                console.log(result);
-
-        } catch(e){
-            console.log(e);
-        }
-
-    }
-
-    render(){
-        return(
-            <>
-                <div className="main-block" id="newform">
-                    <div className="left-part" id="newform">
-                        <i className="fas fa-envelope" id="newform" />
-                        <i className="fas fa-at" id="newform" />
-                        <i className="fas fa-mail-bulk" id="newform" />
-                    </div>
-                    <form id="newform">
-                        <div className="info" id="newform">
-                            <input className="fname" type="text" name="name" placeholder="Full name" id="newform"/>
-                            <input type="text" name="name" placeholder="Email" id="newform"/>
-                            <input type="text" name="name" placeholder="Phone number" id="newform"/>
-                            <input type="text" name="name" placeholder="Website" id="newform"/>
-                        </div>
-                        <div>
-                            <textarea rows="4" id="newform"/>
-                        </div>
-                        <button id="newform" type="submit" href="/">Submit</button>
-                    </form>
-                </div>
-            </>
-        );
-
-    }
-}
-*/
 export default NewForm;
-
